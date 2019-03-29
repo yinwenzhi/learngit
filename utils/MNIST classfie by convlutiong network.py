382fe8fc460a0396b62163414e5ee96b1bf5559c
wenzhiyin <43230896+wenzhiyin@users.noreply.github.com>
2019-03-29 13:01:15 +0800

    modified MNIST classfy file

--- utils/MNIST classfie by convlutiong network.py
+++ utils/MNIST classfie by convlutiong network.py
@@ -7,59 +7,60 @@ import torchvision.transforms
 
 #构建网络结构 
 
-# #第一种形式
-# class Net(torch.nn.Module)
-#   def __init__(self):
-#     super(Net,self).__init__()
-#     self.conv0 = torch.nn.Conv2d(1,64,kernel_size = 3, padding= 1)
-#     self.relu1 = torch.nn.ReLU()
-#     self.conv2 = torch.nn.Conv2d(64, 128, kernel_size = 3, padding= 1)
-#     self.relu3 = torch.nn.ReLU()
-#     self.pool4 = torch.nn.MaxPool2d(stride= 2, kernel_size= 2)
-#     self.fc5 = torch.nn.Linear(128 * 14 * 14, 1024)
-#     self.relu6 = torch.nn.ReLU()
-#     self.drop7 = torch.nn.Dropout(p = 0.5)
-#     self.fc8 = torch.nn.Linear(1024,10)
-
-#   def forward(self,x):
-#     x = self.conv0(x)
-#     x = self.relu1(x)
-#     x = self.conv2(x)
-#     x = self.relu3(x)
-#     x = self.pool4(x)
-#     x = x.view(-1, 128 * 14 * 14)
-#     x = self.fc5(x)
-#     x = self.relu6(x)
-#     x = self.drop7(x)
-#     x = self.fc8(x)
-#     return x 
-# net = Net()
-# net 
-
-#第二种形式 把部分层整合在一起
+#第一种形式
 class Net(torch.nn.Module):
-
   def __init__(self):
-    super(Net, self).__init__()
-    self.conv = torch.nn.Sequential(
-      torch.nn.Conv2d(1, 64,kernel_size = 3,padding= 1),
-      torch.nn.ReLU(),
-      torch.nn.Conv2d(64,128 ,kernel_size = 3, padding = 1),
-      torch.nn.ReLU(),
-      torch.nn.MaxPool2d(stride=2, kernel_size = 2)
-    )
-    self.dense = torch.nn.Sequential(
-      torch.nn.Linear(128 * 14 * 14 ,1024 ),
-      torch.nn.ReLU(),
-      torch.nn.Dropout(p = 0.5),
-      torch.nn.Linear(1024,10)
-    )
-  def forward(self,x):
-    x = self.conv(x)
-    x = x.view(-1, 128 * 14 *14)
-    x = self.dense(x)
+    super(Net,self).__init__()
+    self.conv0 = torch.nn.Conv2d(1,64,kernel_size = 3, padding= 1)
+    self.relu1 = torch.nn.ReLU()
+    self.conv2 = torch.nn.Conv2d(64, 128, kernel_size = 3, padding= 1)
+    self.relu3 = torch.nn.ReLU()
+    self.pool4 = torch.nn.MaxPool2d(stride= 2, kernel_size= 2)
+    self.fc5 = torch.nn.Linear(128 * 14 * 14, 1024)
+    self.relu6 = torch.nn.ReLU()
+    self.drop7 = torch.nn.Dropout(p = 0.5)
+    self.fc8 = torch.nn.Linear(1024,10)
 
+  def forward(self,x):
+    x = self.conv0(x)
+    x = self.relu1(x)
+    x = self.conv2(x)
+    x = self.relu3(x)
+    x = self.pool4(x)
+    x = x.view(-1, 128 * 14 * 14)
+    x = self.fc5(x)
+    x = self.relu6(x)
+    x = self.drop7(x)
+    x = self.fc8(x)
+    return x 
 net = Net()
+net 
+
+# #第二种形式 把部分层整合在一起
+# class Net(torch.nn.Module):
+
+#   def __init__(self):
+#     super(Net, self).__init__()
+#     self.conv = torch.nn.Sequential(
+#       torch.nn.Conv2d(1, 64,kernel_size = 3,padding= 1),
+#       torch.nn.ReLU(),
+#       torch.nn.Conv2d(64,128 ,kernel_size = 3, padding = 1),
+#       torch.nn.ReLU(),
+#       torch.nn.MaxPool2d(stride=2, kernel_size = 2)
+#     )
+#     self.dense = torch.nn.Sequential(
+#       torch.nn.Linear(128 * 14 * 14 ,1024 ),
+#       torch.nn.ReLU(),
+#       torch.nn.Dropout(p = 0.5),
+#       torch.nn.Linear(1024,10)
+#     )
+#   def forward(self,x):
+#     x = self.conv(x)
+#     x = x.view(-1, 128 * 14 *14)
+#     x = self.dense(x)
+#     return x
+
+# net = Net()
 
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters())
@@ -83,7 +84,7 @@ test_loader = torch.utils.data.DataLoader(
   dataset = test_dataset,
   batch_size = batch_size
 )
-print('tranin_dataset',train_dataset)
+# print('tranin_dataset',train_dataset)
 #训练网络
 num_epochs= 5
 for epoch in range(num_epochs): #loop over the dataset mutiple times
@@ -96,8 +97,8 @@ for epoch in range(num_epochs): #loop over the dataset mutiple times
 
       #forward + backward + optimize
       preds = net(input_images)
-      print("preds",preds)
-      print("labels",labels)
+      # print("preds",preds)
+      # print("labels",labels)
 
       loss = criterion(preds,labels)
       loss.backward()
