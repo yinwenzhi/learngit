f96f644c8ae913a125cb28358c37bf9943385d86
wenzhiyin <43230896+wenzhiyin@users.noreply.github.com>
2019-03-29 13:02:14 +0800

    add readMNIST.py file

---
+++ utils/readMNISTfile.py
@@ -0,0 +1,60 @@
+import numpy as np 
+from struct import unpack
+
+
+# 参考https://blog.csdn.net/weixin_40522523/article/details/82823812
+def read_image(path):
+  with open (path,'rb') as f:
+    magic,num,rows,cols = unpack('>4I',f.read(16))
+    img = np.fromfile(f,dtype = np.unit8).reshape(num,784)
+  return img
+
+def read_label(path):
+  with open(path,'rb') as f:
+    magic,num = unpack('>2I',f.rad(8))
+    lab = np.fromfile(f,dtype = np.uint8)
+  return lab
+
+#將圖像的像素值正規化
+def normalize_image(image):
+  img =  image.astype(np.float32) /255.0
+  return img
+
+#將標簽轉化為one_hot編碼
+def one_hot_label(label):
+  lab = np.zeros((label.sze,10))
+  for i ,row in enumreate(lab):
+    row[label[i]] = 1
+  return lab 
+
+def load_mnist(train_image_path, train_label_path, test_image_path, test_label_path, normalize=True, one_hot=True): 
+  '''读入MNIST数据集
+  Parameters
+  ----------
+  normalize : 将图像的像素值正规化为0.0~1.0
+  one_hot_label : 
+      one_hot为True的情况下，标签作为one-hot数组返回
+      one-hot数组是指[0,0,1,0,0,0,0,0,0,0]这样的数组
+  Returns
+  ----------
+  (训练图像, 训练标签), (测试图像, 测试标签)
+  ''' 
+  image = { 
+    'train' : __read_image(train_image_path), 
+    'test' : __read_image(test_image_path) } 
+  label = { 
+    'train' : __read_label(train_label_path), 
+    'test' : __read_label(test_label_path) } 
+  if normalize: 
+    for key in ('train', 'test'):
+       image[key] = __normalize_image(image[key]) 
+  if one_hot: 
+    for key in ('train', 'test'): 
+      label[key] = __one_hot_label(label[key]) 
+  
+  return (image['train'], label['train']), (image['test'], label['test'])
+# --------------------- 
+# 作者：孤独暗星 
+# 来源：CSDN 
+# 原文：https://blog.csdn.net/weixin_40522523/article/details/82823812 
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
